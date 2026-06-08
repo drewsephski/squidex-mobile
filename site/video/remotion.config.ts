82a804ca247fe6f70416d3fc92fa715ec56efe4e
drewsephski <drewsepeczi@gmail.com>
2026-06-08 13:52:05 -0500

    fix vid

--- site/video/remotion.config.ts
+++
@@ -1,7 +0,0 @@
-import {Config} from '@remotion/cli/config';
-
-Config.setVideoImageFormat('jpeg');
-Config.setOverwriteOutput(true);
-
-// Required for WebGL in HtmlInCanvas onPaint
-Config.setChromiumOpenGlRenderer('angle');
